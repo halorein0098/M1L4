14cbff6f22c4ce6cbee820486db66c8f71bc499d
halorein0098 <160326041+halorein0098@users.noreply.github.com>
2024-03-16 18:26:32 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,34 @@
+from settings import settings
+import discord
+from bot_logic import *
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah masuk sebagai {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('$hello'):
+        await message.channel.send('Saya! Saya bot!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTIxMzQ2OTA3MzQ3MjQ5MTU1MA.Gg4WbO.lrB0LEfPDBLh9h68ZlFBOgUfHoaVDKk6-h5nhE")
